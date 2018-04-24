803fc92aa5e5ccf8d3e0386818b69961d453be4e
Programmer Muslim <rizki@imastudio.co.id>
2018-04-24 14:25:36 +0700

    add

--- app/src/main/java/superheroku/id/co/androidkotlin2/MainActivity.kt
+++ app/src/main/java/superheroku/id/co/androidkotlin2/MainActivity.kt
@@ -2,11 +2,31 @@ package superheroku.id.co.androidkotlin2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var username = findViewById(R.id.txtUsername) as EditText
+        var pass = findViewById(R.id.txtPassword) as EditText
+        var btnLogin = findViewById(R.id.btnLogin) as Button
+        btnLogin.setOnClickListener{
+            if(isValidCredential(username.text.toString(),pass.text.toString())){
+                Snackbar.make(btn_login,"Success Login",Snackbar.LENGTH_LONG)
+                        .setAction("Action",null).show()
+            }else{
+                Snackbar.make(btn_login,"Invalid Credential",Snackbar.LENGTH_LONG)
+                        .setAction("Action",null).show()
+            }
+        }
+        }
+
+    fun isValidCredential(email: String, password:String): Boolean{
+        return (email == "developer@coniolabs.com" && password == "Android")
+    }
     }
 }
